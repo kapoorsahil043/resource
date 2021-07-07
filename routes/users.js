3b324847bdf721e2c5ec405d4bbf3ee9f3c23167
kapoorsahil043 <75875511+kapoorsahil043@users.noreply.github.com>
2021-07-07 16:56:33 +0800

    removed imports

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { Otp } = require('../models/otp');
 const multer = require('multer');
 
 const FILE_TYPE_MAP = {
